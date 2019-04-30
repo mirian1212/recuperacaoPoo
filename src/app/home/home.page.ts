c8dab9baf2653b970cafb379acad8929f377a80d
mirian1212 <emelytosta159@gmail>
2019-04-30 15:34:08 -0300

    recuperacao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,13 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  altura: number = 0;
-  basemenor: number = 0;
-  basemaior: number = 0;
-  resultado: string;
+  raio: number;
+  pi: number = 3.14;
+  area: string;
 
   calcular(): void {
-   let area = (Number(this.basemenor) + Number(this.basemaior)) * Number(this.altura) / 2;
-   this.resultado = 'a área do trapezio é:'+area;
+   let area = (Number(this.raio)* Number(this.raio)) * Number(this.pi);
+   this.area = 'a área do circulo é: '+area;
  }
 }
